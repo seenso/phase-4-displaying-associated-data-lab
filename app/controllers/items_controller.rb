0c5a6f88aad85e44016e50270b1c3808f77ca48f
seenso <>
2022-01-12 15:49:10 -0900

    Compeleted

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+  # GET /items
+  def index 
+    render json: Item.all, include: :user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show 
+    user = User.find(params[:id])
+    render json: user, include: :items
+  end
 end
